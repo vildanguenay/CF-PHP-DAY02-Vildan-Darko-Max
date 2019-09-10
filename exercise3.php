0e6bdf71ff02429d643d376cc0e459f2f60f789f
Darko Kolak <kolakdarko@yahoo.com>
2019-09-10 11:03:01 +0200

    done

---
+++ exercise3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exercise3</title>
+</head>
+<body>
+
+
+<?php 
+
+function vildan($a, $b){
+
+	echo "Vildan will " .$a. " millionen euro in " .$b. " Jahren";
+}
+
+vildan(20,1);
+
+
+ ?>
+
+</body>
+</html>
